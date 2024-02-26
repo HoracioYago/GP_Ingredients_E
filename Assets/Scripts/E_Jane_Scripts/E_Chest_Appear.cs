f814fbc1f69dc5f1803e8a303d62b586a2450118
JiniferSim <evsaltsina@gmail.com>
2024-02-26 18:07:42 +0100

    1st scene part 2
    almost finished, fixed the door anim by making interactive rotation 0

--- Assets/Scripts/E_Jane_Scripts/E_Chest_Appear.cs
+++ Assets/Scripts/E_Jane_Scripts/E_Chest_Appear.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class E_Chest_Appear : Interactive
 {
-    Animator animator;
-
+    public Animator animator;
+    public GameObject key;
+    bool Opened;
     public override void OnInteraction()
     {
         animator.SetTrigger("Open");
+        Opened = true;
+        if (Opened == true)
+        {
+            key.SetActive(true);
+        }
+        else
+        {
+            key.SetActive(false);
+        }
     }
 }
