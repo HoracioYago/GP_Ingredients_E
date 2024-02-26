b118d91b598ab0a230d534e5c2dfd03b1914d046
grosspengu1n <Ismailyomid@gmail.com>
2024-02-26 16:08:08 +0100

    [add] teleport

--- Assets/Scripts/E_Omid_Scripts/E_Teleport.cs
+++ Assets/Scripts/E_Omid_Scripts/E_Teleport.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class E_Teleport : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform toTeleport;
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        other.transform.position = toTeleport.position;
     }
 }
